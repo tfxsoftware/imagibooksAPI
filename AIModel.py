ec3b140643e3215b7e2071d47fd4b2393e1f6654
tfxsoftware <aswespeak.tfx@gmail.com>
2024-03-25 14:53:18 -0300

    engine modified

--- AIModel.py
+++ AIModel.py
@@ -2,12 +2,20 @@ from openai import OpenAI
 client = OpenAI()
 
 def generateRecommendation(quiz):
+    tipo = "futurista"
+    genero = "ficção"
+    books = ["star wars", "os vingadores", "sankofia"]
+
+    role = f"Voce é um vendedor de livros, mas faz recomedações de livros que existem no mundo real, sua resposta deve começar com o nome do livro, seguido por um ponto e então a explicação do por que ele seria interessante pra mim"
+    prompt = f"Me recomende um livro do gênero {genero} em um mundo de {tipo}, outros livors que eu li {books[0]}, {books[1]} e {books[2]}"
+
+
     completion = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
-        {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-        {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+        {"role": "system", "content": role},
+        {"role": "user", "content": prompt}
     ]
     )
-    return completion.choices[0].message
+    return completion.choices[0].message.content
 
--- Api.py
+++ Api.py
@@ -1,11 +1,12 @@
-from flask import Flask, jsonify
+
 import ServiceImpl as services
 import Models.QuizAwnser
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 # Define a route for your API endpoint
-@app.route('/api/hello', methods=['GET'])
+@app.route('/ai/recommend', methods=['GET'])
 def hello():
     return jsonify({"message": str(services.recommendAndImage(1))})
 
