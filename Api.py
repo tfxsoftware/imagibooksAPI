9aacecc2c1cbbbc9dc515597f45c84f0d6b2e727
tfxsoftware <aswespeak.tfx@gmail.com>
2024-03-26 14:09:49 -0300

    new user added

--- Api.py
+++ Api.py
@@ -1,14 +1,27 @@
 
 import ServiceImpl as services
 import Models.QuizAwnser
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import firebase_admin
+from firebase_admin import credentials, firestore, initialize_app
+import json
 
 app = Flask(__name__)
+cred = credentials.Certificate("C:\\imagibooks-firebase-adminsdk-6tgdr-99184fb4fc.json")
+firebase_app = initialize_app(cred)
+db = firestore.client()
 
-# Define a route for your API endpoint
 @app.route('/ai/recommend', methods=['GET'])
-def hello():
+def recommend():
     return jsonify({"message": str(services.recommendAndImage(1))})
+
+@app.route('/user/new', methods=['POST'])
+def newUser():
+    
+    data = request.json
+    json_data = json.dumps(data)
+    return services.newUser(db, json_data)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- ServiceImpl.py
+++ ServiceImpl.py
@@ -1,5 +1,17 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
 import AIModel as ai
+from flask import jsonify
+import json
 
 def recommendAndImage(quiz):
     ai.generateRecommendation(quiz)
     return ai.generateRecommendation(quiz)
+
+def newUser(db, data):
+        user = json.loads(data)
+        
+        doc_ref = db.collection('users').add(user)
+        return jsonify({'message': 'Data added to Firestore'}), 200
+
+    
